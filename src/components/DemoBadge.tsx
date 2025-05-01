4c661bcfaa735318830e1e379aedee69282f5591
Jon Dipilato <jon@dipilatoautomations.com>
2025-05-01 19:36:09 -0400

    feat(demobadge): remove boilerplate demo, add Terms link

--- src/components/DemoBadge.tsx
+++ src/components/DemoBadge.tsx
@@ -1,11 +1,11 @@
 export const DemoBadge = () => (
   <div className="fixed bottom-0 right-20 z-10">
     <a
-      href="https://react-saas.com"
+      href="https://leads.dipilatoautomations.com/terms"
     >
       <div className="rounded-md bg-gray-900 px-3 py-2 font-semibold text-gray-100">
-        <span className="text-gray-500">Demo of</span>
-        {' SaaS Boilerplate'}
+        <span className="text-gray-500">Our</span>
+        {' Terms And Conditions'}
       </div>
     </a>
   </div>
